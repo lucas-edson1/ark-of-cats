9a153de1daa5c24b3750f53111d980fdb673ddad
Lucas <lucasedson03@gmail.com>
2024-10-18 00:55:59 -0300

    Alteração da migration de Users padrão e criação da migration User Favorites para trabalhar com o BD

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,30 +11,20 @@ return new class extends Migration
      */
     public function up(): void
     {
+        // nome, e-mail, telefone, CPF e senha. O campo “sexo” é opcional.
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone');
+            $table->string('cpf');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('sexo')->nullable();            
+            $table->string('status')->default('active');
+            $table->timestamp('last_access')->nullable();            
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+        });        
+        
     }
 
     /**
