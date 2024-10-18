73342bc03e8d338f477c2b4171407bafed4c0afd
Lucas <lucasedson03@gmail.com>
2024-10-18 00:57:37 -0300

    Criação de todas as rotas para as requisições do sistema

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GatoController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\CadastroController;
 
-Route::get('/', function () {
-    return view('app.home');
+Route::get('/', function () {return view('app.home');})->name('app.home');
+Route::get('/search', [GatoController::class, 'search'])->name('search.breed');
+Route::post('/favoritar', [GatoController::class, 'favoritar'])->name('favoritar.cat');
+
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'loginCreate'])->name('login.form');
+    Route::post('/login', [LoginController::class, 'auth'])->name('login.auth');
+    Route::get('/cadastro', [CadastroController::class, 'cadastroCreate'])->name('cadastro.form');
+    Route::post('/cadastro', [CadastroController::class, 'cadastrar'])->name('cadastro.save');
 });
+
+Route::get('/logout', [LoginController::class, 'logout'])->name('login.logout');
+Route::get('/perfil', [UserController::class, 'perfil'])->name('user.perfil');
+Route::get('/perfil/favorites', [UserController::class, 'getFavorites'])->name('user.favorites');
+Route::delete('/perfil/favorites/remover/{id}', [UserController::class, 'removeFavorite'])->name('user.removefavorite');
+Route::patch('/perfil/atualizar', [UserController::class, 'atualizar'])->name('user.atualizar');
